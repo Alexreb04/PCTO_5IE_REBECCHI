91ebc28c71332cd40f790edec79f2b7e07534c73
Alexreb04 <beatsalex9@gmail.com>
2023-03-10 12:43:51 +0100

    inizio sviluppo pagina login

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+
+<head>
+<meta charset="utf-8" />
+<title>LOGIN</title>
+<link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+    <div class="colore"></div>
+    <div class="dati">
+    sdfsdfsdf
+    </div>
+</body>
+
+</html>
