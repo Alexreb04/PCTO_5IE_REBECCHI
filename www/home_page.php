9ca6c1506c34d5571a98f564f915641d6eac104a
nessi alessandro <nessi5089@fseveri.it>
2023-04-01 10:54:33 +0200

    subfolder created

--- www/home_page.php
+++ www/home_page.php
@@ -24,9 +24,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>HOME PAGE</title>
+    <link rel="stylesheet" href="HOMEPAGECSS.css">
 </head>
 <body>
+<div class="buttonGrid">
+        <div class="button" onclick="">Button 1</div>
+        <div class="button" onclick="">Button 2</div>
+        <div class="button" onclick="">Button 3</div>
+        <div class="button" onclick="">Button 4</div>
+        <div class="button" onclick="">Button 5</div>
+        <div class="button" onclick="">Button 6</div>
+    </div>
     
 </body>
 </html>
