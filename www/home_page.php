fc2d9cdcbf99cdb8b661b7d8796abfff91968fb1
nessi alessandro <nessi5089@fseveri.it>
2023-04-14 13:06:34 +0200

    Home page variabile

--- www/home_page.php
+++ www/home_page.php
@@ -4,20 +4,26 @@
     $mysqli = require("conn_db.php");
 
     $ruolo = $_SESSION["ruolo"];
+
     $queryAux = 'SELECT R.IDAUT
     FROM RuoloValore as R
     WHERE R.ruolo = "'.$ruolo.'"';
 
     $result = $mysqli -> query($queryAux);
 
+    $is_amministratore = false;
     foreach($result as $v){
         foreach($v as $key => $value) {
-            echo "$key $value <br>";
+            if($value === '1')
+            {
+                $is_amministratore = true;
+            }
         }
     }
-
     
+    session_destroy();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,17 +31,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME PAGE</title>
-    <link rel="stylesheet" href="HOMEPAGECSS.css">
+    <?php if($is_amministratore) : ?>
+        <link rel="stylesheet" href="home_page_admin.css">
+    <?php elseif (!$is_amministratore): ?>
+        <link rel="stylesheet" href="home_page_hp.css">
+    <?php endif; ?>
 </head>
 <body>
-<div class="buttonGrid">
-        <div class="button" onclick="">Button 1</div>
-        <div class="button" onclick="">Button 2</div>
-        <div class="button" onclick="">Button 3</div>
-        <div class="button" onclick="">Button 4</div>
-        <div class="button" onclick="">Button 5</div>
-        <div class="button" onclick="">Button 6</div>
-    </div>
+    <!-- gestore non conformita -->
+    <?php if($is_amministratore) : ?>
+        <div class="buttonGrid">
+            <div class="button" onclick="">Button 1</div>
+            <div class="button" onclick="">Button 2</div>
+            <div class="button" onclick="">Button 3</div>
+            <div class="button" onclick="">Button 4</div>
+            <div class="button" onclick="">Button 5</div>
+            <div class="button" onclick="">Button 6</div>
+        </div>
+    <?php elseif (!$is_amministratore): ?>
+        <div class="container">
+            <div class="buttonGrid">
+                <div class="button" onclick="">Button 1</div>
+                <div class="button" onclick="">Button 2</div>
+                <div class="button" onclick="">Button 3</div>
+            </div>
+            <div class="sottoGriglia">
+                <div class="button largo" onclick="">Button 4</div>
+                <div class="button medio" onclick="">Button 5</div>
+            </div>
+        </div>
+    <?php endif; ?>
     
 </body>
 </html>
